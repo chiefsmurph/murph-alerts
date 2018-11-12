f6aa38ac252d7e5a0324ead240a4197b01a26e92
John Murphy <chiefsmurph@gmail.com>
2018-11-11 16:38:01 -0800

    lets go!

--- alertChange.js
+++ alertChange.js
@@ -1,12 +1,10 @@
 
 const sendEmail = require('./send-email');
 
-
 const templateObj = obj => obj ? Object.keys(obj).reduce((acc, key) => `${acc}${key}: ${obj[key]}\n`, '') : 'no data at this time';
 
 const allEmails = [
     '5102940361@vtext.com',
-    'chiefsmurph@gmail.com',
     '9254081895@msg.fi.google.com'
 ];
 
@@ -21,9 +19,9 @@ module.exports = async ({ name, curVal, prevVal }) => {
             `PREVIOUS VALUE\n----------------\n${templateObj(prevVal)}`,
         ];
         
-        // for (let m of messages) {
-        //     await sendToNumber(m);
-        // }
+        for (let m of messages) {
+            await sendToNumber(m);
+        }
     
     };
 
